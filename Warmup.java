f62d2c33cb165d701b84bba59ef087fd37c28a6f
perto <noahp@post.bgu.ac.il>
2020-04-08 13:59:09 +0300

    version 1

--- Warmup.java
+++ Warmup.java
@@ -1,17 +1,15 @@
 public class Warmup {
     public static int backtrackingSearch(int[] arr, int x, int fd, int bk, Stack myStack) {
         int countSteps = 0; //counts number of steps
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i = i + 1) {
             myStack.push(arr[i]);
-            if (x == arr[i]) {
+            if (x == arr[i])
                 return i;
-                break;
-            }
             countSteps++;
             if (countSteps == fd) { // fd steps were made
                 i = i - bk;
                 countSteps = 0;
-                for (int j = 0; j < bk; j--) { //clearing bk elements from the stack
+                for (int j = 0; j < bk; j = j - 1) { //clearing bk elements from the stack
                     myStack.pop();
                 }
             }
@@ -20,25 +18,22 @@ public class Warmup {
     }
 
     public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
-        int r = arr.length - 1;
-        int l = 0;
-        while (r >= l) {
-            int mid = (r + l) / 2;
+        int right = arr.length - 1;
+        int left = 0;
+        while (right >= left) {
+            int mid = (right + left) / 2;
             myStack.push(arr[mid]);
             while (isConsistent(arr) != 0) // the array is not consistent
                 myStack.push();
-            if (arr[mid] == x) { // if x was found
+            if (arr[mid] == x) // if x was found
                 return mid;
-                break;
-            }
-            if (arr[mid] < x) { // x is bigger than the element in arr[mid]
-                l = mid + 1;
-             else // x is smaller than the element in arr[mid]
-                r = mid - 1;
-            }
+            if (arr[mid] < x)  // x is bigger than the element in arr[mid]
+                left = mid + 1;
+            else // x is smaller than the element in arr[mid]
+                right = mid - 1;
         }
-        return -1; // x was not found
-    }
+        return-1; // x was not found
+}
 
     private static int isConsistent(int[] arr) {
         double res = Math.random() * 100 - 75;
